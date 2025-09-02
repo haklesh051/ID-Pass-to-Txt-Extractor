53bd652dd6d26bb230ec889bfc00381e1a38889e
haklesh051 <hakleshbairwa051@gmail.com>
2025-09-02 07:32:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =27730810
+api_hash = "dadc82d593e3f5334da107b63ced36b9"
+bot_token = "8285234513:AAF2qu6Kfj2MovCBXRDE2PlYPeXRx-mc0BY"
+auth_users = "6132794263"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "6132794263"
 owner_users = [int(num) for num in osowner_users.split(",")]
