5e6097f14648c131d572d92a458cf9763835e23a
haklesh051 <hakleshbairwa051@gmail.com>
2025-09-02 12:22:53 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-api_id =27730810
-api_hash = "dadc82d593e3f5334da107b63ced36b9"
-bot_token = "8285234513:AAF2qu6Kfj2MovCBXRDE2PlYPeXRx-mc0BY"
-auth_users = "6132794263"
-sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6132794263"
-owner_users = [int(num) for num in osowner_users.split(",")]
